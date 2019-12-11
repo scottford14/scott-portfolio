9dfb59cbf84b06216dc2e561d1e471337ad6a080
scottford14 <scottford14@gmail.com>
2019-12-10 20:28:27 -0500

    Added pictures to BCM and description

--- src/components/ProjectPage/BCM.js
+++ src/components/ProjectPage/BCM.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react'
 import ProjectPage from './ProjectPage';
 
-import mainpic from './../../img/bcm/BCM Render.png';
+import render from './../../img/bcm/BCM Render.png';
+import bcmblank from './../../img/bcm/BCM Blank.jpg';
+import enclosure from './../../img/bcm/BCM Enclosure Render.png';
+import schematic from './../../img/bcm/BCM Top Level Schematic.png';
+import assembly from './../../img/bcm/BCM Front Assembled.jpg';
+
 
 export class BCM extends Component {
     render() {
@@ -9,9 +14,17 @@ export class BCM extends Component {
             <ProjectPage
             title="Body Control Module"
             images={[
-                {image: mainpic,
+                {image: render,
+                description:"Altium Rendering of the PCB after completing the layout"},
+                {image: bcmblank,
+                description:""},
+                {image: enclosure,
+                description:""},
+                {image: schematic,
+                description:""},
+                {image: assembly,
                 description:""}]}
-            description=""
+            description="This board is the main board that controls all systems of the car, appart from the motors. The board has 2 microcontrollers for parellel processing of vehicle data, such as pedal position, steering angle, etc. The board also houses safety circuitry for the car, as well as circuitry for the dashboard display, and for CAN communication, the protocal used for communication between the vehicles various systems. The board is located in the front end of the car, in an aluminum enclosure with a fan for active cooling of the power electronics."
             />
         )
     }
