d2db3e248e7a62ca270138e6257b3352a88787f2
scottford14 <scottford14@gmail.com>
2019-12-11 21:20:41 -0500

    Added descriptions to shutdown circuit

--- src/components/ProjectPage/Shutdown.js
+++ src/components/ProjectPage/Shutdown.js
@@ -13,12 +13,12 @@ export class Shutdown extends Component {
             title="Shutdown Circuitry"
             images={[
                 {image: vehicleshutdown,
-                description:""},
+                description:"The vehicle shutdown circuit ensures driver safety during operation of the vehicle"},
                 {image: chargershutdown,
-                description:""},
+                description:"The charger shutdown circuit ensures that the charger can be safely disabled in the event of a BMS fault the disconnecting of the HV connection"},
                 {image: testing,
-                description:""}]}
-            description="The shutdown circuit is a critical safety measure that incorperates various interlocks to deactivate the vehciles tractive system incase of safety risk or errors. "
+                description:"Test setup used to verify components of the shutdown circuit working in operation together"}]}
+            description="The shutdown circuit is a critical safety circuit that incorperates various interlocks to deactivate the vehicles tractive system incase of safety risk or system errors. The circuit directly drives two relays located in the HV battery pack that can disconnect the tractive system completely, isolating the driver and spectators from potential risks. Some of the circuits include the Brake System Plausibility Device, which is an analog circuit that checks for hard breaking and positive current delivered to the motors, which prevents erroneous acceleration in the case of a stuck accelerator or CAN message corruption. The shutdown circuit would also be triggered if there is any faults with the BMS, both during charging of the battery pack and driving. Many other interlocks are present as well, which can be seen in the schematics above."
             />
         )
     }
