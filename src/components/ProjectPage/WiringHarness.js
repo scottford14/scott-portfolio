5a3db5c4d0506e7cf64472dca30887d81f657dc4
scottford14 <scottford14@gmail.com>
2019-12-11 19:37:18 -0500

    Added more pictures to wiring and descriptions

--- src/components/ProjectPage/WiringHarness.js
+++ src/components/ProjectPage/WiringHarness.js
@@ -2,7 +2,10 @@ import React, { Component } from 'react'
 
 import ProjectPage from './ProjectPage';
 
-import mainpic from './../../img/Wiring Harness/Wiring Harness.jpg';
+import tablepic from './../../img/Wiring Harness/Wiring Harness.jpg';
+import connections from './../../img/Wiring Harness/connections.png';
+import inverterback from './../../img/Wiring Harness/Inverter Wiring Back.jpg';
+import dash from './../../img/Wiring Harness/Dashboard.jpg';
 
 export class WiringHarness extends Component {
     render() {
@@ -10,9 +13,15 @@ export class WiringHarness extends Component {
             <ProjectPage
             title="Low Voltage and High Voltage Vehicle Wiring"
             images={[
-                {image:mainpic,
-                description:""}]}
-            description=""
+                {image:tablepic,
+                description:"Image of the wiring harness laid out prior to wiring up inside of vehicle"},
+                {image:connections,
+                description:"Schematic illustrating all the connections made on the vehicle wiring harness, originating at the Body Control Module"},
+                {image:inverterback,
+                description:"Showing the HV wiring connections from rear motors to inverters, and some of the low voltage wiring associated with the motor controllers"},
+                {image:dash,
+                description:"Drivers view of the dashboard, with fault status lights and shutdown button on the left, low voltage battery indicator and start button on the right, and 6 steering wheel mounted buttons"}]}
+            description="The wiring within the vehicle required extensive design, given the large number of low voltage connections needed to implement all of the required circuitry for vehicle safety, vehicle controls, cooling, CAN Communication, driver cockpit buttons and warning lights, power delivery, and more. Extensive high voltage connections were needed to, supplying power to all 4 wheel mounted 3 phase motors, high voltage measuring points, and tractive system discharge circuitry."
             />
         )
     }
