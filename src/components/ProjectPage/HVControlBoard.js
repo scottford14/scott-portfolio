4e32cd20d14096d25a5825da15da4e1f7cacaa79
scottford14 <scottford14@gmail.com>
2020-04-07 20:26:51 -0400

    Added pics for HV Control Board and LV BCM

--- src/components/ProjectPage/HVControlBoard.js
+++ src/components/ProjectPage/HVControlBoard.js
@@ -4,6 +4,8 @@ import ProjectPage from './ProjectPage';
 
 import render from './../../img/HV Control Board/HV Control Board Render Black.PNG';
 import drawing from './../../img/HV Control Board/HV Control Board in drawing.PNG';
+import top from './../../img/HV Control Board/HV Control Board top.jpg';
+import side from './../../img/HV Control Board/HV Control Board side.jpg';
 
 export class HVControlBoard extends Component {
     render() {
@@ -14,7 +16,11 @@ export class HVControlBoard extends Component {
                 {image: render,
                 description:"Altium rendering of the PCB after completing the layout"},
                 {image: drawing,
-                description:"Board location shown in full vehicle assembly, see red highlight. Drawing done by Jared Cox - Mechanical Team Captain"}]}
+                description:"Board location shown in full vehicle assembly, see red highlight. Drawing done by Jared Cox - Mechanical Team Captain"},
+                {image: top,
+                description:""},
+                {image: side,
+                description:""}]}
             description="The HV Control Board is a PCB housing 3 core circuits, the tractive system discharge circuit, tractive system active light, and HV measuring point resistors. These circuits are the only HV circuits outside of the battery pack, due to limited space inside of the battery pack to meet packaging requirements. I designed each circuit to work with our vehicles 600V tractive system, ensuring that all components were rated for the maximum bus voltage. I formulated the circuits from direct requirements from the SAE rules, such as the discharge circuit requiring the tractive system capacitors to discharge below 60V in under 5 seconds. I had to find out what discharge resistance to use that could handle the maximum power dissipation during the discharge event, given the bus capacitance and the bus voltage, while meeting the timing requirement. Once all circuits were designed and tested with an HV power supply, I proceeded with the board layout, ensuring HV spacing requirements were met, traces were an adequate width for maximum current, as well as ensuring it would work in terms of packaging."
             />
         )
--- src/components/ProjectPage/LVBCM.js
+++ src/components/ProjectPage/LVBCM.js
@@ -3,6 +3,8 @@ import ProjectPage from './ProjectPage';
 
 import render from './../../img/lvbcm/LV BCM Render.PNG';
 import drawing from './../../img/lvbcm/LV BCM in drawing.PNG';
+import top from './../../img/lvbcm/LV BCM top.jpg';
+import side from  './../../img/lvbcm/LV BCM side.jpg';
 
 export class LVBCM extends Component {
     render() {
@@ -13,7 +15,11 @@ export class LVBCM extends Component {
                 {image: render,
                 description:"Altium rendering of the PCB after completing the layout"},
                 {image: drawing,
-                description:"Board location shown in full vehicle assembly, see red highlight. Drawing done by Jared Cox - Mechanical Team Captain"}]}
+                description:"Board location shown in full vehicle assembly, see red highlight. Drawing done by Jared Cox - Mechanical Team Captain"},
+                {image: top,
+                description:""},
+                {image: side,
+                description:""}]}
             description=""
             />
         )
