7e0ce3accfc1a71a0889402998270609ee6583bb
scottford14 <scottford14@gmail.com>
2020-04-07 20:01:13 -0400

    Added pic links for LVandHV ACB

--- src/components/ProjectPage/LVandHVACB.js
+++ src/components/ProjectPage/LVandHVACB.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react'
 import ProjectPage from './ProjectPage';
 
-import render from './../../img/lvandhvacb/so-close.JPG';
+import upclose from './../../img/lvandhvacb/so-close.JPG';
+import hvrender from './../../img/lvandhvacb/HV ACB Render.PNG';
+import lvrender from './../../img/lvandhvacb/LV ACB Render.PNG';
+import close from './../../img/lvandhvacb/close.JPG';
+import iso from './../../img/lvandhvacb/iso.JPG';
+import top from './../../img/lvandhvacb/top.JPG';
+import front from './../../img/lvandhvacb/front.JPG';
 
 
 export class LVandHVACB extends Component {
@@ -10,8 +16,20 @@ export class LVandHVACB extends Component {
             <ProjectPage
             title="LV and HV Accumulator Control Board"
             images={[
-                {image: render,
-                description:"Altium rendering of the PCB after completing the layout"}]}
+                {image: upclose,
+                description:"Altium rendering of the PCB after completing the layout"},
+                {image: hvrender,
+                description:""},
+                {image: lvrender,
+                description:""},
+                {image: close,
+                description:""},
+                {image: iso,
+                description:""},
+                {image: top,
+                description:""},
+                {image: front,
+                description:""}]}
             description=""
             />
         )
